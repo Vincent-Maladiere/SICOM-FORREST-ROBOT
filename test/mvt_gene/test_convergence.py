6c532ea0aac587ab799274c9f1170b10c7837105
G.Destouet <g.destouet@hotmail.fr>
2017-03-21 09:35:27 +0100

    modifie le test de convergence

--- test/mvt_gene/test_convergence.py
+++ test/mvt_gene/test_convergence.py
@@ -6,15 +6,21 @@ from mvt_gene.genetics import *
 
 #############Definitions#############
 ####Variables####
+##A priori les variables ci-dessous seront determinés par la pratique##
 mvt_nb = 3 # nombre de mouvements
+nb_run = 50 # nombre de simulation
+size_i = 10 # taille d'un individu
+
+##Les 3 variables ci-dessous seront celles à optimiser pour atteindre le plus rapidement la convergence##
+
 proba_mut = 0.1 # probabilite de mutation
 size_g = 10 # taille d'une generation
-size_i = 10 # taille d'un individu
 nb_ind_slt = 3 # nombre d'individu selectionné chaque generation
-nb_run = 50 # nombre de simulation
-nb_run_mean = 50 # nombre de lancement afin d'estimer la convergence en moyenne
 
 
+nb_run_mean = 50 # nombre de lancement afin d'estimer la convergence en moyenne, elle devrait au moins être supérieur à la moyenne + variance 
+
+##Ci dessous la fonction d'evaluation définissant l'individu optimal##
 def evaluation(liste) :
     return sum(liste)
 
@@ -33,6 +39,7 @@ def run_algogene():
 
 #############Debut du script#############
 nb_opt = [] # Vecteur de numeros de la generation ayant atteint la solution optimale
+filename = None # Nom de fichier de sorti
 
 for i in sys.argv[1:] :
     exec(i)
@@ -68,20 +75,23 @@ print()
 print('#############')
 print('Convergence en moyenne :')
 print()
-
+nb_opt = []
 for i in range(nb_run_mean) :
     gen.ran_gen(mvt_nb)
     eval_gene(gen)
     nb_opt+=[run_algogene()]
 nb_conv = 0
-
 for i in range(nb_opt.count(0)) : nb_opt.remove(0)
 nb_conv = len(nb_opt)
-mean_conv = sum(nb_opt)/nb_conv
-print('Avec nb_run_mean :',nb_run_mean,'| Proba_mut :',proba_mut,' | nb_run :',nb_run,' | nb_ind_slt :',nb_ind_slt)
-print('La convergence est atteinte en moyenne a la ', int(mean_conv),' generation')
-print(nb_conv,' generations ont convergées')
-print('Maximum : ',max(nb_opt),' Minimum : ', min(nb_opt) )
+print('Avec nb_run_mean :',nb_run_mean,'| proba_mut=',proba_mut,' | nb_run=',nb_run,' | nb_ind_slt=',nb_ind_slt)
+print('size_g=',size_g,'| size_i=',size_i,'| mvt_nb=',mvt_nb) 
+if nb_conv > 0 :
+    mean_conv = sum(nb_opt)/nb_conv
+    print('La convergence est atteinte en moyenne a la ', int(mean_conv),' generation')
+    print(nb_conv,' generations ont convergées')
+    print('Maximum : ',max(nb_opt),' Minimum : ', min(nb_opt) )
+else : print('Aucune generation n a atteint la solution optimale')
+
 if nb_conv > 1 :
     nb_conv_var = sum( [ (i-mean_conv)**2 for i in nb_opt ] )/(nb_conv-1)
     print('Variance :', math.sqrt(nb_conv_var))
